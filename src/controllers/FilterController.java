7e455b302d9302e1cacae1ad68ce4938a6b14a5f
Ayuzvik <ayuzvik@wpi.edu>
2017-11-11 15:53:00 -0500

    Filter screen now has functioning check boxes tied to icons on map

--- src/controllers/FilterController.java
+++ src/controllers/FilterController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.*;
 
 public class FilterController implements ControllableScreen{
     private ScreenController parent;
@@ -21,9 +23,47 @@ public class FilterController implements ControllableScreen{
     @FXML
     private Button btncancel;
 
+    @FXML
+    private ImageView bathIcon1;
+
+    @FXML
+    private ImageView exitIcon1;
+    @FXML
+    private ImageView exitIcon2;
+
+    @FXML
     public void cancelPressed(ActionEvent e)
     {
         System.out.println("Cancel Pressed");
         parent.setScreen(ScreenController.MainID);
     }
+    @FXML
+    public void bathRoomChecked(ActionEvent e)
+    {
+        if(checkBoxBathrooms.isSelected())
+        {
+            bathIcon1.setOpacity(1.0);
+            System.out.println("bathroom is checked");
+        }
+        else
+        {
+            bathIcon1.setOpacity(0.0);
+        }
+    }
+    @FXML
+    public void exitChecked(ActionEvent e)
+    {
+        if(checkBoxExits.isSelected())
+        {
+            exitIcon1.setOpacity(1.0);
+            exitIcon2.setOpacity(1.0);
+            System.out.println("exits is checked");
+        }
+        else
+        {
+            exitIcon1.setOpacity(0.0);
+            exitIcon2.setOpacity(0.0);
+        }
+    }
+
 }
